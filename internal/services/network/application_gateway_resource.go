ddbe7f5e78302ab902eaeb462980851163fc5019
Matthew Frahry <mbfrahry@gmail.com>
2022-01-25 10:21:09 -0800

    Attempt to lintignore

--- internal/services/network/application_gateway_resource.go
+++ internal/services/network/application_gateway_resource.go
@@ -166,8 +166,8 @@ func resourceApplicationGateway() *pluginsdk.Resource {
 			},
 
 			// Required
+			//lintignore:S016
 			"backend_address_pool": {
-				//lintignore:S016
 				Type: func() pluginsdk.ValueType {
 					if features.ThreePointOh() {
 						return pluginsdk.TypeSet
